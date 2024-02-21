aafbdb8b04bd3d4298446c25914c9c228d0d81d0
Sebastián Zapata <63472538+sebastianzapatar@users.noreply.github.com>
2024-02-21 11:08:43 -0500

    Se crea el diccionario de operaciones

--- lenguaje/lexer.py
+++ lenguaje/lexer.py
@@ -1,7 +1,8 @@
 from re import match
 from lenguaje.token import(
     Token,
-    TokenType
+    TokenType,
+    lookup_token_type
 )
 """
 El lexer es le que identifica
@@ -35,13 +36,56 @@ class Lexer:
         while self._current_char.isspace():
             self._read_character() 
     """
+    peek_character leer el siguiente caracter 
+    sin necesidad de avanzar en el cursor
+    """
+    def _peek_character(self)->str:
+        if self._read_current_pos >= len(self._source):
+            return ""
+        else:
+            return self._source[self._read_current_pos]
+    """
+    Para verificar que el caracter es una letra, soporta español
+    """
+    def is_letter(self,character:str)->bool:
+        return bool(match(r'^[a-záéíóúA-ZÁÉÍÓÚñÑ_]$',character))
+    """
+    Para evaluar si es un número
+    el d indica si \d significa que es un número
+    """
+    def is_number(self,character:str)->bool:
+        return bool(match(r'^\d$',character))
+    """
+    Función para leer y devolver identenficadores
+    """
+    def _read_identifier(self)->str:
+        initial_position=self._current_pos
+        is_first_letter=True
+        while self.is_letter(self._current_char) or \
+         (not is_first_letter and self.is_number(self._current_char)):
+            self._read_character()
+            is_first_letter=False
+        return self._source[initial_position:self._current_pos]
+    """
+    Funcion para retornar si es un número
+    """
+    def _read_number(self):
+        initial_positon=self._current_pos
+        while self.is_number(self._current_char):
+            self._read_character()
+        return self._source[initial_positon:self._current_pos]
+    """
     Evalua cada token y devuelve su tipo
     """
     def next_token(self):
         
         self._skip_whitespace()
         if match(r'^=$', self._current_char):
-            token = Token(TokenType.ASSIGN, self._current_char)
+            if self._peek_character() == "=":
+                self._read_character()
+                token = Token(TokenType.EQ, "==")
+            else:
+                token = Token(TokenType.ASSIGN, self._current_char)
         elif match(r'^\+$', self._current_char):
             token = Token(TokenType.PLUS, self._current_char)
         elif match(r'^,$', self._current_char):
@@ -50,6 +94,31 @@ class Lexer:
             token = Token(TokenType.SEMICOLON, self._current_char)
         elif match(r'^$', self._current_char):
             token = Token(TokenType.EOF, self._current_char)
+        elif match(r'^>$',self._current_char):
+            if self._peek_character() == "=":
+                self._read_character()
+                token = Token(TokenType.GTE, ">=")
+            else:
+                token = Token(TokenType.GT, self._current_char)
+        elif match(r'^<$',self._current_char):
+            if self._peek_character() == "=":
+                self._read_character()
+                token = Token(TokenType.LTE, "<=")
+            else:
+                token = Token(TokenType.LT, self._current_char)
+        elif match(r'^!$',self._current_char):
+            if self._peek_character() == "=":
+                self._read_character()
+                token = Token(TokenType.NOE, "!=")
+            else:
+                token = Token(TokenType.NOT, self._current_char)
+        elif self.is_letter(self._current_char):
+            literal=self._read_identifier()
+            token_type=lookup_token_type(literal)
+            return Token(token_type,literal)
+        elif self.is_number(self._current_char):
+            literal=self._read_number()
+            return Token(TokenType.INT,literal)
         else:
             token = Token(TokenType.ILLEGAL, self._current_char)
         self._read_character()
--- lenguaje/token.py
+++ lenguaje/token.py
@@ -15,30 +15,41 @@ Cada token es único y su identificador no se repite
 class TokenType(Enum):
     ASSIGN=auto()#=
     COMMA=auto()#,
+    DIVISION=auto()
+    ELSE=auto()
     EOF=auto()#nada
+    EQ=auto()#==
+    FALSE=auto()
+    FUNCTION=auto()
+    GT=auto()#>
+    GTE=auto()#>=
+    IDENT=auto()#identificador de una variable
+    IF=auto()
     ILLEGAL=auto()#?´~
+    INT=auto()#int
+    LET=auto()#Let para asignar variables
+    LT=auto()#<
+    LTE=auto()#<=
+    NOE=auto()#!=
+    NOT=auto()#!
     PLUS=auto()#+
+    RETURN= auto()
     SEMICOLON=auto()#;
-    """
-    {
-    }
-    [
-    ]
-    -
-    /
-    (
-    )
-    ^
-    !
-    >
-    <
-    """
-"""
-Mostrar el token
-"""
+    TRUE= auto()
 class Token(NamedTuple):
     token_type:TokenType
     literal:str
 
     def __str__(self) -> str:
         return f'{self.token_type.name}:{self.literal}'
+def lookup_token_type(literal:str)->TokenType:
+    keywords:Dict[str,TokenType]={
+        'false':TokenType.FALSE,
+        'true':TokenType.TRUE,
+        'function':TokenType.FUNCTION,
+        'return':TokenType.RETURN,
+        'if':TokenType.IF,
+        'else':TokenType.ELSE,
+        'let':TokenType.LET
+    }
+    return keywords.get(literal,TokenType.IDENT)
